eda7c420c432565eb76d6af22d099b3222f13a41
Jenkins Automation Server <jenkins@ip-172-31-29-43.ec2.internal>
2018-12-17 00:43:18 +0000

    adding CoreNodesRunning Alarm

--- scripts/create_emr_monitoring.py
+++ scripts/create_emr_monitoring.py
@@ -18,14 +18,42 @@ def check_arg(args=None):
     results = parser.parse_args(args)
     return (results.ClusterID,results.ClusterName)
  
-def main():
-    ClusterID, ClusterName = check_arg(sys.argv[1:])
-    client = boto3.client('cloudwatch')
+#globals
+ClusterID, ClusterName = check_arg(sys.argv[1:])
+client = boto3.client('cloudwatch')
+
+def create_dashboard():
     dashboardTemplate='{"widgets":[{"type":"metric","x":0,"y":0,"width":24,"height":3,"properties":{"view":"singleValue","metrics":[["AWS/ElasticMapReduce","TotalLoad","JobFlowId","j-2V3IPUFKMUUIR"],[".","MRActiveNodes",".","."],[".","MemoryTotalMB",".","."],[".","CoreNodesRunning",".","."],[".","CoreNodesPending",".","."]],"region":"us-east-1"}}]}'
 
     response = client.put_dashboard(
         DashboardName = ClusterName,
         DashboardBody = dashboardTemplate
     )
+
+def create_CoreNodesRunning_alarm():
+    AlarmNameString = ClusterName + ' Core Nodes Running Violation'
+    client.put_metric_alarm(
+        AlarmName=AlarmNameString,
+        ComparisonOperator='LessThanThreshold',
+        EvaluationPeriods=1,
+        MetricName='CoreNodesRunning',
+        Namespace='AWS/ElasticMapReduce',
+        Period=60,
+        Statistic='Minimum',
+        Threshold=2,
+        ActionsEnabled=True,
+        AlarmDescription='Insufficient number of Core Nodes Running',
+        AlarmActions=['arn:aws:sns:us-east-1:994386103535:oncall'],
+        Dimensions=[
+            {
+                'Name': 'JobFlowId',
+                'Value': ClusterID 
+            },
+        ],
+    )
+
+def main():
+    create_dashboard()
+    create_CoreNodesRunning_alarm()
 
 main()
